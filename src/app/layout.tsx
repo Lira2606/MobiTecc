07e89d088a0fafeaec4951f225b2bd3a195f9cc4
mateus lira <mateuslira996@gmail.com>
2025-08-02 00:38:55 +0000

    coloque templates com cores claros e bonitos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={cn('min-h-screen bg-background font-sans antialiased dark', inter.variable)}>
+      <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
         {children}
         <Toaster />
       </body>
