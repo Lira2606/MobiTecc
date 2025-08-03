21f3ee340585445b24b79efb9af411629ec2d58b
mateus lira <mateuslira996@gmail.com>
2025-08-03 15:35:31 +0000

    substitua a tela de login atual por essa <!DOCTYPE html> <html lang="pt-

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,5 @@
 import { LoginForm } from '@/components/login-form';
 
 export default function LoginPage() {
-  return (
-    <div className="flex flex-col flex-grow h-full p-6 justify-center">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 }
